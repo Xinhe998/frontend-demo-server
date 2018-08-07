2fb78420cca8779116b32048fac4a8b5bf5f3f95
Xinhe <ren7722c@gmail.com>
2018-08-07 18:41:11 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -8,14 +8,17 @@ router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
+/* 註冊 */
 router.post('/register', function (req, res, next) {
   var db = req.con;
   let result = {};
   //加密
+  var salt = crypto.randomBytes(128).toString('base64');
   let password = req.body.Password;
-  let hashPassword = crypto.createHash('sha1');
+  let hashPassword = crypto.createHmac('sha256', salt);
   hashPassword.update(password);
   const rePassword = hashPassword.digest('hex');
+  
 
   const memberData = {
     Email: req.body.Email,
@@ -40,7 +43,7 @@ router.post('/register', function (req, res, next) {
     }
   })
 });
-
+/* 檢查Email重複 */
 router.post('/checkEmailIsExist', function (req, res, next) {
   var db = req.con;
   let result = {};
@@ -71,5 +74,49 @@ router.post('/checkEmailIsExist', function (req, res, next) {
     }
   })
 })
+/* 登入 */
+router.post('/login', function (req, res, next) {
+  var db = req.con;
+  let result = {};
+
+  const loginData = {
+    Email: req.body.Email,
+    Password: rePassword
+  }
+  console.log(rePassword);
+  //console.log(req.body);
+  db.query('SELECT * FROM member WHERE Email = ? AND Password = ?', [loginData.Email, loginData.Password], function (err, rows) {
+    if (err) {
+      console.log(err);
+      result.status = "註冊失敗。"
+      result.err = "伺服器錯誤，請稍後在試！"
+      res.json({
+        result: err
+      })
+    } else {
+      console.log(rows);
+      if (rows.length >= 1) {
+        // 產生token
+        const token = jwt.sign({
+          algorithm: 'HS256',
+          exp: Math.floor(Date.now() / 1000) + (60 * 60), // token一個小時後過期。
+          data: rows[0].Email
+        }, config.secret);
+        res.setHeader('token', token);
+
+        result.status = "登入成功。"
+        res.json({
+          result: result
+        })
+      } else {
+        result.status = "登入失敗。"
+        result.err = "請輸入正確的帳號或密碼。";
+        res.json({
+          result: result
+        })
+      }
+    }
+  })
+});
 
 module.exports = router;
